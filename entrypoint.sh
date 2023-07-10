82e79237c6147264e213c8f80cb66642ded1737b
peter <p.makretskii@gmail.com>
2023-07-10 13:06:18 +0300

    Supplement README.md

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,7 @@ export IS_DOCKER_COMPOSE=1
 cd IIS
 python manage.py migrate
 python manage.py loaddata fixtures/users.json
+printf "\n\n\t\tsuperuser created:\n\t\t\tlogin - admin\n\t\t\tpassword - 8Xgex7E6CRCF4wz\n\n\n"
 python manage.py runserver 0.0.0.0:8000
 
 exec $cmd
